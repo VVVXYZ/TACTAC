86732f69d623ed80a6fb7190a785549c5e92243e
zfning <771070906@qq.com>
2016-07-29 09:58:38 +0800

    框架

---
+++ src/main/java/com/trio/breakFast/controller/HomepageController.java
@@ -0,0 +1,12 @@
+package com.trio.breakFast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by asus on 2016/7/29.
+ */
+@Controller
+@RequestMapping("/homepage")
+public class HomepageController extends BaseController {
+}
